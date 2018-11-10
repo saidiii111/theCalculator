2d9190464a1a09d62bbbdbf13475f64737b83e60
said <alsaghir.said@gmail.com>
2018-11-10 18:54:00 +0100

    adding eventListener to display the clicked num, decimal and add is-depressed class to make operator active when it's clicked

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
-
+// adding addEventListener and use dataset property to define specific action
 keys.addEventListener("click", e => {
   if (e.target.matches("button")) {
     const key = e.target;
@@ -27,6 +27,35 @@ keys.addEventListener("click", e => {
     }
     if (action === "calculate") {
       console.log("equal key!");
+    }
+  }
+});
+// adding addEventListener onClick to show the clicked num using textContent property
+const display = document.querySelector(".calculator__display");
+keys.addEventListener("click", e => {
+  if (e.target.matches("button")) {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    // adding the logic to add a clicked num instead for default 0 num, and if another num get append
+    if (!action) {
+      if (displayedNum === "0") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+    if (action === "decimal") {
+      display.textContent = displayedNum + ".";
+    }
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      key.classList.add("is-depressed");
     }
   }
 });
